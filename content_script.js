17405e6eed51b14c3b1aa207c82f6b1c6daf61b2
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2023-12-10 22:51:32 -0800

    infrastructure for handling storage changes

--- content_script.js
+++ content_script.js
@@ -1,7 +1,16 @@
 chrome.storage.onChanged.addListener((changes, area) => {
-  console.log('Storage Changed');
+  const newState = changes?.state?.newValue;
+  switch (newState) {
+    case 'DISABLED':
+      console.log('time to disable extension');
+      break;
+    case 'BREAK_FROM_SCREEN':
+      console.log('time to take a break from the screen');
+      break;
+    case 'RETURN_TO_SCREEN':
+      console.log('time to return to screen');
+      break;
+  }
 });
 
-window.onload = () => {
-  console.log('HELLO?');
-};
+window.onload = () => {};
