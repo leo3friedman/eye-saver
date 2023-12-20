3eab468f056a48fb27a764284b124a5776d0bd20
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2023-12-20 00:03:16 -0800

    add skip button

--- eyeSaver/overlay.js
+++ eyeSaver/overlay.js
@@ -1,15 +1,15 @@
+let eyeSaver
+
 const main = async () => {
   const eyeSaverSrc = await import(chrome.runtime.getURL('eyeSaver.js'))
-  const eyeSaver = new eyeSaverSrc.EyeSaver(chrome, onResting, onBreaking)
-  // console.log('restIfPossible onload!')
-  // eyeSaver.restIfPossible()
+  eyeSaver = new eyeSaverSrc.EyeSaver(chrome, onResting, onBreaking)
+
   eyeSaver.handleCurrentState()
 }
 
-const onResting = (restDurationRemaining) => {
+const onResting = () => {
   if (!isOverlayOn()) {
-    addCanvas()
-    // setTimeout(removeCanvas, restDurationRemaining)
+    addOverlay()
   }
 }
 
@@ -28,14 +28,16 @@ const isOverlayOn = () => {
   return document.querySelectorAll('.eye-saver__overlay').length > 0
 }
 
-const addCanvas = () => {
-  const canvas = document.createElement('div')
-  const dropzone = document.createElement('dropzone')
-  canvas.className = 'eye-saver__overlay'
-  dropzone.className = 'timer__dropzone'
-  dropzone.style.display = 'block'
+const applyStyles = (node, styles) => {
+  Object.keys(styles).forEach((key) => {
+    node.style[key] = styles[key]
+  })
+}
 
-  const styles = {
+const addOverlay = () => {
+  const overlay = document.createElement('div')
+  overlay.className = 'eye-saver__overlay'
+  applyStyles(overlay, {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -46,14 +48,28 @@ const addCanvas = () => {
     left: '0',
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     zIndex: Number.MAX_SAFE_INTEGER,
-  }
-
-  Object.keys(styles).forEach((key) => {
-    canvas.style[key] = styles[key]
   })
 
-  canvas.appendChild(dropzone)
-  document.body.appendChild(canvas)
+  const overlayContents = document.createElement('div')
+  applyStyles(overlayContents, {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px',
+  })
+
+  const dropzone = document.createElement('dropzone')
+  dropzone.className = 'timer__dropzone'
+
+  const skipButton = document.createElement('button')
+  skipButton.innerText = 'SKIP'
+  skipButton.onclick = async () => {
+    await eyeSaver.setSessionStart()
+  }
+
+  overlay.appendChild(overlayContents)
+  overlayContents.appendChild(dropzone)
+  overlayContents.appendChild(skipButton)
+  document.body.appendChild(overlay)
   renderClock(dropzone)
 }
 
@@ -66,11 +82,6 @@ const renderClock = async (dropzone) => {
   const restDuration = await eyeSaver.getRestDuration()
   const running = await eyeSaver.isExtensionRunning()
   const timePassed = restDuration - (await eyeSaver.getRestDurationRemaining())
-
-  const getGlobalTimePassed = async () => {
-    return await eyeSaver.getCurrentProgress()
-  }
-  // const timePassed = await eyeSaver.getCurrentProgress()
 
   const timer = new timerSrc.Timer(
     restDuration,
